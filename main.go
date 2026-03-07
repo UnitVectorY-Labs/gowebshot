114b3b44c2dd8cae3b9ab62122a4b554731661a8
Jared Hatfield <jjhatf02@gmail.com>
2026-03-06 19:57:33 -0500

    Add main package and initial version handling logic

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "runtime/debug"
+
+var Version = "dev" // This will be set by the build systems to the release version
+
+func main() {
+	// Set the build version from the build info if not set by the build system
+	if Version == "dev" || Version == "" {
+		if bi, ok := debug.ReadBuildInfo(); ok {
+			if bi.Main.Version != "" && bi.Main.Version != "(devel)" {
+				Version = bi.Main.Version
+			}
+		}
+	}
+
+	// TODO Implement everything
+}
